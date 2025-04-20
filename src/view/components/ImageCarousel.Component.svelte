569011132bf285416b5d2b296e3a70bd9dafde09
Ruben Ipenza <ruben.ipenza@jala.university>
2025-04-20 11:52:07 -0400

    feat: add movement logic

--- src/view/components/ImageCarousel.Component.svelte
+++ src/view/components/ImageCarousel.Component.svelte
@@ -6,17 +6,27 @@
 	let currentIndex = 0;
 	let defaultImagesToShow = imagesToShow;
 	let visibleImagesToShow = defaultImagesToShow;
+
+	function moveSlide(direction) {
+		currentIndex = (currentIndex + direction + images.length) % images.length;
+	}
 </script>
 
 <div id={containerId}>
 	<div class="carousel-container">
-		<button class="carousel-btn prev">❮</button>
+		<button class="carousel-btn prev" on:click={() => moveSlide(-1)}>❮</button>
 		<section class="carousel-section">
 			{#each images as src, index}
-				<img {src} alt="Imagen {index + 1}" class="carousel-image" />
+				<img
+					{src}
+					alt="Imagen {index + 1}"
+					class="carousel-image"
+					class:active={index >= 0 && index < visibleImagesToShow}
+					class:hidden={index < 0 || index >= visibleImagesToShow}
+				/>
 			{/each}
 		</section>
-		<button class="carousel-btn next">❯</button>
+		<button class="carousel-btn next" on:click={() => moveSlide(1)}>❯</button>
 	</div>
 </div>
 
