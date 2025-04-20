1c15bea5a05fd52a11f2446410d80937f9e400bc
Ruben Ipenza <ruben.ipenza@jala.university>
2025-04-20 11:47:51 -0400

    feat: : create ImageCarousel component with basic structure

---
+++ src/view/components/ImageCarousel.Component.svelte
@@ -0,0 +1,24 @@
+<script>
+	export let containerId;
+	export let images;
+	export let imagesToShow = 5;
+
+	let currentIndex = 0;
+	let defaultImagesToShow = imagesToShow;
+	let visibleImagesToShow = defaultImagesToShow;
+</script>
+
+<div id={containerId}>
+	<div class="carousel-container">
+		<button class="carousel-btn prev">❮</button>
+		<section class="carousel-section">
+			{#each images as src, index}
+				<img {src} alt="Imagen {index + 1}" class="carousel-image" />
+			{/each}
+		</section>
+		<button class="carousel-btn next">❯</button>
+	</div>
+</div>
+
+<style>
+</style>
