225c86d502ee0499f14be23688d220d85bdeb9d5
Ruben Ipenza <ruben.ipenza@jala.university>
2025-04-27 18:47:22 -0400

    feat: create initial banner component structure

---
+++ src/view/components/Banner.Component.svelte
@@ -0,0 +1,11 @@
+<div id={containerId}>
+	<div class="banner-container">
+		<button class="banner-btn prev" on:click={() => moveSlide(-1)}>❮</button>
+		<img
+			src={displayedImage.src}
+			alt={displayedImage.alt}
+			class="banner-image"
+		/>
+		<button class="banner-btn next" on:click={() => moveSlide(1)}>❯</button>
+	</div>
+</div>
