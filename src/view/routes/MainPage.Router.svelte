c9393a10ea0e72eeeec8268e7d2f662a8e3d10b4
Dog-Mangy <edwin.chacon@jala.university>
2025-04-27 16:29:12 -0500

    feat: add Footer component

--- src/view/routes/MainPage.Router.svelte
+++ src/view/routes/MainPage.Router.svelte
@@ -1,3 +1,15 @@
+<script>
+  import Footer from '../components/Footer.Component.svelte';
+</script>
+
 <h1>MainPage</h1>
 
 <a href="/mainPage">MainPage</a>
+
+<main>
+
+
+<!-- Instancias el Footer aquí -->
+<Footer />
+
+</main>
