6652909bf755807955897308ab9b11694f9373bc
Ruben Ipenza <ruben.ipenza@jala.university>
2025-04-27 19:01:43 -0400

    feat: add banner component script logic

--- src/view/components/Banner.Component.svelte
+++ src/view/components/Banner.Component.svelte
@@ -1,3 +1,19 @@
+<script>
+	export let containerId;
+	export let images;
+
+	let currentIndex = 0;
+
+	$: displayedImage = {
+		src: images[currentIndex],
+		alt: `Banner Image ${currentIndex + 1}`,
+	};
+
+	function moveSlide(direction) {
+		currentIndex = (currentIndex + direction + images.length) % images.length;
+	}
+</script>
+
 <div id={containerId}>
 	<div class="banner-container">
 		<button class="banner-btn prev" on:click={() => moveSlide(-1)}>❮</button>
