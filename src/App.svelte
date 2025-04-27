fe95d82f87282c62c455dd3565c20ac480710ad2
Dog-Mangy <edwin.chacon@jala.university>
2025-04-27 15:49:48 -0500

    feat: add Footer component

--- src/App.svelte
+++ src/App.svelte
@@ -2,22 +2,25 @@
 	import { Router, Route } from 'svelte-routing';
 	import { routes } from './view/routes/Router.js';
 	import Header from './view/components/Header.svelte';
+	import Footer from './view/components/Footer.svelte';
   
 	export let url = '';
   
 	// Simulamos el role. Luego podríamos traerlo de login real.
 	let userRole = 'buyer'; // Cambia a 'buyer', 'seller', etc.
-  </script>
-  
-  <main>
+</script>
+
+<main>
 	<!-- Aquí ponemos el header y le pasamos el userRole -->
 	<Header {userRole} />
   
 
 	<Router {url}>
-	  {#each routes as { path, component }}
-		<Route {path} {component} />
-	  {/each}
+		{#each routes as { path, component }}
+			<Route {path} {component} />
+		{/each}
 	</Router>
-  </main>
-  
+
+	<!-- Footer siempre abajo -->
+	<Footer />
+</main>
