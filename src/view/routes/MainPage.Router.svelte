17fa3daec9b94c80cdcf459a592b3466fbdb5ee1
Ruben Ipenza <ruben.ipenza@jala.university>
2025-04-27 23:05:42 -0400

    fix: mainpage

--- src/view/routes/MainPage.Router.svelte
+++ src/view/routes/MainPage.Router.svelte
@@ -1,15 +1,19 @@
 <script>
-  import Footer from '../components/Footer.Component.svelte';
+	import Header from '../components/Header.svelte';
+	import Footer from '../components/Footer.Component.svelte';
+
+	// We simulate the role. We could then bring it in from the real login.
+	let userRole = 'buyer'; // Change it to 'buyer', 'seller', etc.
 </script>
 
-<h1>MainPage</h1>
-
-<a href="/mainPage">MainPage</a>
+<!-- Here we put the header and pass it the userRole -->
+<Header {userRole} />
 
 <main>
+	<h1>MainPage</h1>
 
-
-<!-- Instancias el Footer aquí -->
-<Footer />
-
+	<a href="/mainPage">MainPage</a>
 </main>
+
+<!-- Instances the Footer here -->
+<Footer />
