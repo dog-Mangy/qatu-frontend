0e39e4db186cb90d0f5de041af088631df09963a
Ruben Ipenza <ruben.ipenza@jala.university>
2025-05-03 00:37:26 -0400

    fix: delete svelte config

--- eslint.config.js
+++ eslint.config.js
@@ -1,7 +1,6 @@
 import js from "@eslint/js";
 import svelte from "eslint-plugin-svelte";
 import globals from "globals";
-import svelteConfig from "./svelte.config.js";
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
@@ -23,7 +22,6 @@ export default [
         // By doing so, some rules in eslint-plugin-svelte will automatically read the configuration and adjust their behavior accordingly.
         // While certain Svelte settings may be statically loaded from svelte.config.js even if you don’t specify it,
         // explicitly specifying it ensures better compatibility and functionality.
-        svelteConfig,
       },
     },
   },
