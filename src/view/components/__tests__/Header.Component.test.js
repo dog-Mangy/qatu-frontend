6fff622764e767ce1bb7fac6c8ab2ea9783a2d1b
Leonardo Camacho <leonardo.camacho@jala.university>
2025-05-04 19:11:03 -0400

    feat: Add simple tests for Header component

---
+++ src/view/components/__tests__/Header.Component.test.js
@@ -0,0 +1,20 @@
+import { render, screen } from '@testing-library/svelte';
+import Header from '../Header.svelte';
+
+describe('Header Component', () => {
+  it('should render logo', () => {
+    render(Header, { props: { userRole: 'user' } });
+    expect(screen.getByAltText('Qatu Logo')).toBeInTheDocument();
+  });
+
+  it('should show search input on desktop', () => {
+    window.innerWidth = 1024;
+    render(Header);
+    expect(screen.getByPlaceholderText('Search..')).toBeInTheDocument();
+  });
+
+  it('should render navigation', () => {
+    render(Header);
+    expect(screen.getByRole('navigation')).toBeInTheDocument();
+  });
+});
