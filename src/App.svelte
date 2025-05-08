93153e0a8682473d780ed6395b8fa94183379ace
Ruben Ipenza <ruben.ipenza@jala.university>
2025-05-08 16:03:27 -0400

    fix:  fix implementation of routes

--- src/App.svelte
+++ src/App.svelte
@@ -1,25 +1,19 @@
 <script>
 	import './../public/global.css';
 
-	import { Router, Route } from 'svelte-routing';
-	import { routes } from './view/routes/Router.js';
+	import Router from "svelte-spa-router";
+	import routes from './view/routes/Router.js';
 
 	import Header from './view/components/Header.svelte';
 	import Footer from './view/components/Footer.Component.svelte';
 
-	export let url = '';
 	let userRole = 'buyer';
 </script>
 
 <div id="app">
 	<Header {userRole} />
-
 	<main>
-		<Router {url}>
-			{#each routes as { path, component }}
-				<Route {path} {component} />
-			{/each}
-		</Router>
+		<Router {routes}/>
 	</main>
 
 	<Footer />
--- src/view/routes/Router.js
+++ src/view/routes/Router.js
@@ -3,9 +3,11 @@ import Register from './Register.Router.svelte';
 import MainPage from './MainPage.Router.svelte'
 import Profile from './Profile.Router.svelte';
 
-export const routes = [
-  { path: '/profile', component: Profile}, 
-  { path: '/login', component: Login },
-  { path: '/register', component: Register },
-  { path: '/', component: MainPage },
-];
+const routes = {
+  '/profile': Profile, 
+  '/login': Login,
+  '/register': Register,
+  '/': MainPage,
+}
+
+export default routes;
