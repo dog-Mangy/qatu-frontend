da19c52d972c4775f911f59bc61e3848232700f4
ELR1CH4RD <julian.moreno@jala.university>
2025-05-16 14:35:51 -0500

    fix: feat: Implementation of inactivity warning message before closing the session

--- src/lib/viewmodel/utils/inactivityLogout.js
+++ src/lib/viewmodel/utils/inactivityLogout.js
@@ -1,5 +1,8 @@
 let timeoutId;
-const INACTIVITY_LIMIT = 1 * 60 * 1000; // 15 minutos
+let warningTimeoutId;
+let inactividadMostrada = false;
+const WARNING_LIMIT = 1 * 60 * 1000; // 1 minutos
+const INACTIVITY_LIMIT = 3 * 60 * 1000; // 15 minutos
 
 export function startInactivityWatcher(logoutCallback) {
   resetTimer();
@@ -19,15 +22,28 @@ export function startInactivityWatcher(logoutCallback) {
 
   function resetTimer() {
     clearTimeout(timeoutId);
+    clearTimeout(warningTimeoutId);
+    inactividadMostrada = false;
+
+    warningTimeoutId = setTimeout(() => {
+      if (!inactividadMostrada) {
+        alert('Ha empezado el conteo de inactividad. Si permaneces inactiv@ por 15 minutos, se cerrará la sesión automáticamente.');
+        inactividadMostrada = true;
+      }
+    }, WARNING_LIMIT);
+
     timeoutId = setTimeout(() => {
+      inactividadMostrada = false;
       logoutCallback();
     }, INACTIVITY_LIMIT);
   }
 
   return () => {
     clearTimeout(timeoutId);
+    clearTimeout(warningTimeoutId);
     events.forEach(event =>
       window.removeEventListener(event, resetTimer, true)
     );
+    inactividadMostrada = false;
   };
 }
