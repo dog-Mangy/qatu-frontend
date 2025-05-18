ec4c34e5748e166350dbe8d463d5be1eb117258f
Dog-Mangy <edwin.chacon@jala.university>
2025-05-18 17:53:06 -0500

    feat: add the router for categories

--- src/lib/view/routes/Router.js
+++ src/lib/view/routes/Router.js
@@ -2,8 +2,11 @@ import Login from './Login.Router.svelte';
 import Register from './Register.Router.svelte';
 import MainPage from './MainPage.Router.svelte';
 import Profile from './Profile.Router.svelte';
+import Category from './Admin.Router.svelte';
+
 
 const routes = {
+  '/admin': Category,
   '/profile': Profile,
   '/login': Login,
   '/register': Register,
