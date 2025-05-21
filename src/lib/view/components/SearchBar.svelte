e4648f1d75c618b5da63d6233e0b44d72d634b28
Ruben Ipenza <ruben.ipenza@jala.university>
2025-05-21 04:31:54 -0400

    refactor: remove the hook implementation and implement the filter store

--- src/lib/view/components/SearchBar.svelte
+++ src/lib/view/components/SearchBar.svelte
@@ -1,43 +1,17 @@
 <script>
-  import { createEventDispatcher, onMount } from 'svelte';
-
-  const dispatch = createEventDispatcher();
-  let searchQuery = '';
-
-  // Initialize searchQuery from URL on mount
-  onMount(() => {
-    const params = new URLSearchParams(window.location.search);
-    searchQuery = params.get('searchQuery') || '';
-  });
-
-  function updateURL() {
-    const params = new URLSearchParams(window.location.search);
-    if (searchQuery) {
-      params.set('searchQuery', searchQuery);
-    } else {
-      params.delete('searchQuery');
-    }
-    window.history.pushState(
-      {},
-      '',
-      `${window.location.pathname}?${params.toString()}`
-    );
-  }
+  import { filters } from '../../viewmodel/store/filterStore';
 
   function handleInput(event) {
-    searchQuery = event.target.value;
-    updateURL();
-    dispatch('search', { query: searchQuery });
+    filters.update(current => ({
+      ...current,
+      searchQuery: event.target.value,
+    }));
   }
 
   function handleKeydown(event) {
     if (event.key === 'Enter') {
-      handleSearch();
+      window.location.assign('/#');
     }
-  }
-
-  function handleSearch() {
-    dispatch('search', { query: searchQuery });
   }
 </script>
 
@@ -46,13 +20,10 @@
     type="text"
     placeholder="Search.."
     class="search-input"
-    bind:value={searchQuery}
+    bind:value={$filters.searchQuery}
     on:input={handleInput}
     on:keydown={handleKeydown}
   />
-  <button class="search-icon" on:click={handleSearch} aria-label="Search">
-    <i class="fa fa-search"></i>
-  </button>
 </div>
 
 <style>
@@ -72,29 +43,6 @@
     border: 1px solid #ccc;
     outline: none;
     box-sizing: border-box;
-  }
-
-  .search-icon {
-    position: absolute;
-    top: 42.5%;
-    right: 12px;
-    transform: translateY(-50%);
-    background-color: #f1f1f1;
-    border-radius: 50%;
-    padding: 6px;
-    width: 20px;
-    height: 20px;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    font-size: 12px;
-    color: gray;
-    cursor: pointer;
-  }
-
-  .search-icon:hover {
-    background-color: #1a0c46;
-    color: #f1f1f1;
   }
 
   @media screen and (max-width: 600px) {
