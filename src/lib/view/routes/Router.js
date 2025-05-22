6eca85ec95df276293ee9f070fd6dab1f0f10c2f
Dog-Mangy <edwin.chacon@jala.university>
2025-05-22 01:14:42 -0500

    refactor: change the field name

--- src/lib/view/routes/Router.js
+++ src/lib/view/routes/Router.js
@@ -2,11 +2,11 @@ import Login from './Login.Router.svelte';
 import Register from './Register.Router.svelte';
 import MainPage from './MainPage.Router.svelte';
 import Profile from './Profile.Router.svelte';
-import Category from './Admin.Router.svelte';
+import Category from './Category.Router.svelte';
 
 
 const routes = {
-  '/admin': Category,
+  '/categories': Category,
   '/profile': Profile,
   '/login': Login,
   '/register': Register,
