82f6b8db7ee3fc5f4f643080dfcd5ea25206c75d
Leonardo Camacho <leonardo.camacho@jala.university>
2025-05-25 23:38:22 -0400

    feat: Add view of average rating and amount of ratings

---
+++ src/lib/view/components/StoreRatingInfo.svelte
@@ -0,0 +1,16 @@
+<script>
+  export let storeRating;
+</script>
+
+<div class="rating-container">
+  <div class="rating-header">
+    <div class="rating-average">
+      <span class="rating-value">{storeRating.average.toFixed(2)}</span>
+      <span class="rating-max">/5</span>
+    </div>
+    <div class="rating-count">
+      {storeRating.totalRatings} Ratings
+    </div>
+  </div>
+
+</div>
