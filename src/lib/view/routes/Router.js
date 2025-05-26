86efe0fd593ff5fd5116f05feb1823b288e6ba18
Dog-Mangy <edwin.chacon@jala.university>
2025-05-25 21:27:27 -0500

    feat: add the form router

--- src/lib/view/routes/Router.js
+++ src/lib/view/routes/Router.js
@@ -2,11 +2,13 @@ import Login from './Login.Router.svelte';
 import Register from './Register.Router.svelte';
 import MainPage from './MainPage.Router.svelte';
 import Profile from './Profile.Router.svelte';
+import FormRequest from './FormRequest.svelte';
 
 const routes = {
   '/profile': Profile,
   '/login': Login,
   '/register': Register,
+  '/formrequest': FormRequest,
   '/': MainPage,
 };
 
