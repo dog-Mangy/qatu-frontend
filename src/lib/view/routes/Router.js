deb1fb6c13b39c27a1916ab3a14e100f8af64e91
Leonardo Camacho <leonardo.camacho@jala.university>
2025-05-25 23:25:59 -0400

    feat: Add mystore route

--- src/lib/view/routes/Router.js
+++ src/lib/view/routes/Router.js
@@ -2,12 +2,14 @@ import Login from './Login.Router.svelte';
 import Register from './Register.Router.svelte';
 import MainPage from './MainPage.Router.svelte';
 import Profile from './Profile.Router.svelte';
+import MyStore from './MyStore.Router.svelte';
 
 const routes = {
   '/profile': Profile,
   '/login': Login,
   '/register': Register,
   '/': MainPage,
+  '/mystore': MyStore
 };
 
 export default routes;
