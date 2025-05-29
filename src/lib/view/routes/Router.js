d6e38205b722166089643546f3a08ae727bd28ce
Dog-Mangy <edwin.chacon@jala.university>
2025-05-29 18:14:36 -0500

    feat: add the router for the admin request

--- src/lib/view/routes/Router.js
+++ src/lib/view/routes/Router.js
@@ -6,6 +6,8 @@ import Category from './Category.Router.svelte';
 import ProductDetail from '../components/ProductDetail.Component.svelte';
 import FormRequest from './FormRequest.svelte';
 import MyStore from './MyStore.Router.svelte';
+import AdminRequest from './AdminRequest.Router.svelte';
+
 
 const routes = {
   '/categories': Category,
@@ -15,7 +17,8 @@ const routes = {
   '/formrequest': FormRequest,
   '/': MainPage,
   '/store/:id_Store/product/:id': ProductDetail,
-  '/mystore': MyStore
+  '/mystore': MyStore,
+  '/requests' : AdminRequest
 };
 
 export default routes;
