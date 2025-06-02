5cf2d2cc9947b93bf59302d47e9b0a845f4830cd
Ruben Ipenza <ruben.ipenza@jala.university>
2025-06-02 19:30:16 -0400

    refactor: remove filters localStorage

--- src/lib/viewmodel/store/filterStore.js
+++ src/lib/viewmodel/store/filterStore.js
@@ -1,22 +1,14 @@
 import { writable } from 'svelte/store';
 
-const persistedFilters = writable(
-  JSON.parse(localStorage.getItem('filters')) || {
-    searchQuery: '',
-    category: '',
-    minPrice: '0',
-    maxPrice: '1000',
-    minRating: 0,
-    maxRating: 1000,
-    sortBy: 'CreatedAt',
-    ascending: true,
-    page: 1,
-    pageSize: 8,
-  }
-);
-
-persistedFilters.subscribe(value => {
-  localStorage.setItem('filters', JSON.stringify(value));
+export const filters = writable({
+  searchQuery: '',
+  category: '',
+  minPrice: '',
+  maxPrice: '',
+  minRating: '',
+  maxRating: '',
+  sortBy: 'CreatedAt',
+  ascending: true,
+  page: 1,
+  pageSize: 8,
 });
-
-export const filters = persistedFilters;
